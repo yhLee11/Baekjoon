0500b53b8f09413902283468b5ba61c0f6ddb35d
yeonheelee <mrncd1@gmail.com>
2021-08-19 09:57:38 +0900

    dfs

--- 2644.py
+++ 2644.py
@@ -3,29 +3,20 @@ from collections import deque
 sys.setrecursionlimit(100000)
 input=sys.stdin.readline
 cnt=0
-stk=[]
-def dfs(x,y):
-    q=deque()
-    q.append((x,0))
-    while q:
-        cur,dis=q.popleft()
-        if cur==b:
-            return dis
-        for next in dic[cur]:
-            if next==a:
-                continue
-            if visit[next]:
-                continue
-            visit[next]=1
-            q.append((next,dis+1))
-    return -1
-
+def dfs(node):
+    for n in mat[node]:
+        if visit[n]==0:
+            # print('visit[n]',visit[n],'visit[node]',visit[node])
+            visit[n]=visit[node]+1
+            dfs(n)
 n=int(input())
 p1,p2=map(int,input().split())
 m=int(input())
-dic={}
+mat=[[] for _ in range(n+1)]
 visit=[0 for _ in range(n+1)]
 for i in range(m):
     p,c=map(int,input().split())
-    dic[p].append(c)
-    dic[c].append(p)
+    mat[p].append(c)
+    mat[c].append(p)
+dfs(p1)
+print(visit[p2] if visit[p2]>0 else -1)
