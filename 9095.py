5d2cf46493746c72ce6b45823119ef1a7b6a98e8
yeonheelee <mrncd1@gmail.com>
2021-08-24 21:36:01 +0900

    dp

--- 9095.py
+++ 9095.py
@@ -2,4 +2,24 @@ import sys
 input=sys.stdin.readline
 t=int(input())
 num=[int(input()) for _ in range(t)]
-lst=[1,2,3]
+# print(num)
+def sol(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    elif n==3:
+        return 4
+    else:
+        return sol(n-1)+sol(n-2)+sol(n-3)
+for i in num:
+    print(sol(i))
+
+"""
+1: 1->1
+2: 1+1,2->2
+3: 3,1+1+1,1+2,2+1->4
+4: 1+1+1+1,1+1+2,1+3,2+2...->7
+5: 1+1+1+1+1,1+1+1+2,1+1+3,1+2+2,2+2+1,3+2....->13
+점화식: n이 3보다 클 때, f(n)=f(n-1)+f(n-2)+f(n-3)
+"""
