de7cf191f60e70e4efa0fc472c71ccd438e2ebe9
yeonheelee <mrncd1@gmail.com>
2021-08-31 09:08:21 +0900

    sort and greedy

--- 1931.py
+++ 1931.py
@@ -1,21 +1,17 @@
 import sys
-n=int(sys.stdin.readline())
+input=sys.stdin.readline
+n=int(input())
 lst=[]
+for _ in range(n):
+    time=list(map(int,input().split()))
+    start,end=time[0],time[1]
+    lst.append([start,end])
+lst.sort(key=lambda x: [x[1],x[0]])
+
 cnt=0
-start,end=map(int, sys.stdin.readline().split())
-term=end-start
-lst.append((term,start,end))
-
-for i in range(0,n-1):#0,1,2,3...9 // n10전까지
-    start,end=map(int, sys.stdin.readline().split())
-    term=end-start
-
-    if lst[cnt][2]<=start and lst[cnt][1]<=end:#새로 들어온게 안겹치면
-        lst.append((term,start,end))
+end=0
+for i,v in enumerate(lst):
+    if v[0]>=end:
         cnt+=1
-    else:#겹치면
-        if lst[cnt][0]>term:
-            lst.pop(cnt)
-            lst.append((term,start,end))
-print(lst)
-print(cnt+1)
+        end=v[1]
+print(cnt)
