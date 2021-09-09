8fb75bbb7e3f46ee7e630a0c5b4f506df2f81d33
yhLee11 <mrncd1@gmail.com>
2021-09-10 00:47:02 +0900

    bipatite

--- 1707.py
+++ 1707.py
@@ -1,2 +1,29 @@
 import sys
+sys.setrecursionlimit(10**6)
 input=sys.stdin.readline
+k=int(input())
+
+def dfs(v,g):
+    visit[v]=g
+    for i in g[v]:
+        if visit[i]==0:
+            if not dfs(i,-g):
+                return False
+        elif visit[i]==visit[v]:
+            return False
+    return True
+
+for _ in range(k):
+    v,e=map(int,input().split())
+    g=[[] for i in range(v+1)]
+    visit=[0]*(v+1)
+    for _ in range(e):
+        a,b=map(int,input().split())
+        g[a].append(b)
+        g[b].append(a)
+    isBipatite=True
+    for i in range(1,v+1):
+        if visit[i]==0:
+            isBipatite=dfs(i,1)
+            if not isBipatite:break
+    print('YES' if isBipatite else 'NO')
