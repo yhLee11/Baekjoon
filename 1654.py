720ce9b0cd6e5ad432550fcb3ca1ff8b3df29394
yeonheelee <mrncd1@gmail.com>
2021-12-10 13:14:30 +0900

    이분탐색

--- 1654.py
+++ 1654.py
@@ -1,1 +1,16 @@
-k,n = map(int,input().split())
+import sys
+input=sys.stdin.readline
+k,n=map(int,input().split())
+lans=[int(input().strip()) for _ in range(k)]
+start,end=1,max(lans)
+
+while start<=end:
+    mid=(start+end)//2
+    lines=0
+    for i in lans:
+        lines+=i//mid#잘린 랜선 수
+    if lines>=n:
+        start=mid+1
+    else:
+        end=mid-1
+print(end)
