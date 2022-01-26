f81a3f8a23e2e81677d3114d6220dd690377c537
yhLee11 <mrncd1@gmail.com>
2022-01-27 00:31:28 +0900

    dfs

--- 11725.py
+++ 11725.py
@@ -1,25 +1,22 @@
 import sys
-sys.setrecursionlimit(10**9)#재귀의 깊이
+sys.setrecursionlimit(10**9)
 input=sys.stdin.readline
 n=int(input())
-
-tree={}
-parents={}
-for i in range(n):
-    tree[i+1]=[]
-    parents[i+1]=-1
-
-for i in range(n-1):
-    n1,n2=map(int,input().split())
-    tree[n1].append(n2)
-    tree[n2].append(n1)
-
+tree=[[] for _ in range(n+1)]
+parents=[0 for _ in range(n+1)]
+for _ in range(n-1):
+    a,b=map(int,input().split())
+    tree[a].append(b)
+    tree[b].append(a)
+print(tree)
+parents[1]=1
 def dfs(start,tree,parents):
-    for i in tree[start]:#연결된 노드 모두 탐색
-        if parents[i]==-1:#한번도 방문한 적 없으면
-            parents[i]=start#부모노드 저장
+    for i in tree[start]:
+        if parents[i]==0:
+            parents[i]=start
+            print(start,tree[start],i,parents)
             dfs(i,tree,parents)
-
 dfs(1,tree,parents)
+
 for i in range(2,n+1):
     print(parents[i])
