bc3474f0cf1a912690621cda507c2200d9481764
yhLee11 <mrncd1@gmail.com>
2022-01-27 14:45:25 +0900

    bfs dfs

--- 1260.py
+++ 1260.py
@@ -1,38 +1,43 @@
-from collections import deque, defaultdict
+from collections import deque
 import sys
 input=sys.stdin.readline
 
 N,M,V=map(int,input().split())
-graph=defaultdict(list)
-for i in range(M):
-    start,end=map(int,input().split())
-    graph[start].append(end)
-    graph[end].append(start)
+node=[[] for _ in range(N+1)]
+dfs_visit=[0 for _ in range(N+1)]
+bfs_visit=[0 for _ in range(N+1)]
+dfs_res,bfs_res=[],[]
+dfs_res.append(V)
+bfs_res.append(V)
 
-def dfs(start):#V,하나부터 깊게 탐색
-    stk=deque(sorted(graph[start],reverse=True))#리스트 들어감
-    visit=[]
-    visit.append(start)
-    # print(visit,stk)
-    while stk:
-        node=stk.pop()
-        if node not in visit:#방문한적없고,
-            visit.append(node)
-            stk+=sorted(list(set(graph[node])-set(visit)),reverse=True)
-            # print(node,stk)
-    return visit
+for _ in range(M):
+    a,b=map(int,input().split())
+    node[a].append(b)
+    node[b].append(a)
+
+def dfs(start):
+    for i in sorted(node[start]):
+        if dfs_visit[i]==0:
+            dfs_visit[i]=True
+            dfs_res.append(i)
+            # print(i,node[start])
+            dfs(i)
 
 def bfs(start):
-    que=deque(sorted(graph[start]))
-    visit=[]
-    visit.append(start)
-    # print(visit,que)
-    while que:
-        node=que.popleft()
-        if node not in visit:
-            visit.append(node)
-            que+=sorted(list(set(graph[node])-set(visit)))
-            # print(node,que)
-    return visit
-print(*dfs(V))
-print(*bfs(V))
+    dq=deque(sorted(node[start]))
+    while dq:
+        # print(dq)
+        n=dq.popleft()
+        if not bfs_visit[n]:
+            bfs_res.append(n)
+            bfs_visit[n]=True
+            dq.extend(sorted(node[n]))
+            
+# print(node)
+dfs_visit[V]=True
+dfs(V)
+print(*dfs_res)
+
+bfs_visit[V]=True
+bfs(V)
+print(*bfs_res)
