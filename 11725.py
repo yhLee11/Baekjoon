4f72492155e61de475fdd0e87b62866d59af5e98
yhLee11 <mrncd1@gmail.com>
2022-01-27 12:26:04 +0900

    dfs

--- 11725.py
+++ 11725.py
@@ -1,22 +1,20 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10**5)
 input=sys.stdin.readline
 n=int(input())
 tree=[[] for _ in range(n+1)]
-parents=[0 for _ in range(n+1)]
 for _ in range(n-1):
     a,b=map(int,input().split())
     tree[a].append(b)
     tree[b].append(a)
-print(tree)
+parents=[0 for _ in range(n+1)]
 parents[1]=1
-def dfs(start,tree,parents):
+def dfs(start):
     for i in tree[start]:
         if parents[i]==0:
             parents[i]=start
-            print(start,tree[start],i,parents)
-            dfs(i,tree,parents)
-dfs(1,tree,parents)
-
-for i in range(2,n+1):
-    print(parents[i])
+            dfs(i)
+dfs(1)
+parents=parents[2:]
+for i in parents:
+    print(i)
