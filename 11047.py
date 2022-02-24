a97557f89415aa8f5f6328cb992cc0ddc84cd6a3
yhLee11 <mrncd1@gmail.com>
2022-02-24 15:31:51 +0900

    greedy

--- 11047.py
+++ 11047.py
@@ -1,29 +1,15 @@
 import sys
 input=sys.stdin.readline
 n,k=map(int,input().split())
-coin=[int(input()) for _ in range(n)]
-coin.reverse()
+coin=list(reversed([int(input()) for _ in range(n)]))
 cnt=0
 
-for i in range(n):
-    if coin[i]==k:
-        print(1)
-        break
-    elif coin[i]<k:
-
-        m=-1
-        n1=-1
-        while i<=n:
-            if n1==0:break
-            if m==0 and n<0:
-                k+=n1
-                i+=1
-
-            m=k//coin[i]#몫
-            n1=k%coin[i]#나머지
-            k=n1#현재금액
-
-            cnt+=m
-            m=n1
-            i+=1
+for c in coin:
+    if k==0:
+        print(cnt)
+        exit(0)
+    a,b=divmod(k,c)
+    if a!=0:#몫이 있는 경우
+        cnt+=a
+        k=b
 print(cnt)
