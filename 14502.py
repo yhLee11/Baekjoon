cdf1a07a502c72f6b1b165958664be90d109ba5f
yhLee11 <mrncd1@gmail.com>
2022-03-01 23:36:23 +0900

    바이러스 풀이중

--- 14502.py
+++ 14502.py
@@ -1,10 +1,8 @@
 from collections import deque
 import sys
 input=sys.stdin.readline
+n,m=map(int,input().split())
 dx=[1,-1,0,0]
 dy=[0,0,1,-1]
 
-def bfs(i,j):
-    q=deque()
-    q.append((i,j))
-    
+board=[list(map(int,input().split())) for _ in range(n)]
