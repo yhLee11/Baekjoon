75db26dbcebd7b3210174e4865d326d0fa2bb391
yhLee11 <mrncd1@gmail.com>
2022-03-10 15:37:55 +0900

    pre,in,post order

--- 1991.py
+++ 1991.py
@@ -1,70 +1,38 @@
 import sys
-def Btree(r):
-    return [r,[],[]]
-def insertLeft(root,newBranch):
-    t=root.pop(1)
-    if len(t)>0:
-        root.insert(1,[newBranch,t,[]])
-    else:
-        root.insert(1,[newBranch,[],[]])
-def insertRight(root,newBranch):
-    t=root.pop(2)
-    if len(t)>0:
-        root.insert(2,[newBranch,t,[]])
-    else:
-        root.insert(2,[newBranch,[],[]])
-def setRootVal(root,newVal):
-    root[0]=newVal
-def getLeftChild(root):
-    return root[1]
-def getRightChild(root):
-    return root[2]
-def getRootVal(root):
-    return root[0]
+input=sys.stdin.readline
+n=int(input())
+tree={}
+for _ in range(n):
+    root,left,right=map(str,input().split())
+    tree[root]=[left,right]
 
-n=int(sys.stdin.readline())
-rTree=list(sys.stdin.readline().split())
-r=Btree(rTree[0])
-if rTree[1]!='.':
-    insertLeft(r,rTree[1])
-if rTree[2]!='.':
-    insertRight(r,rTree[2])
-
-for i in range(1,n):
-
-    subTree=list(sys.stdin.readline().split())
-    sub=Btree(subTree[0])
-    if subTree[1]!='.':
-        insertLeft(getLeftChild(r),subTree[1])
-    if subTree[2]!='.':
-        insertRight(getRightChild(r),subTree[2])
-
-print(r)
-aws=''
-def preoder(root):#전위순회
-    if not root:
+def preorder(root):
+    if root=='.':
         return
-    else:
-        print(getRootVal(root),end='')
-        preoder(getLeftChild(root))
-        preoder(getRightChild(root))
+    print(root,end='')
+    lst=tree[root]
+    preorder(lst[0])
+    preorder(lst[1])
 
-def inorder(root):#중위순회
-    if not root:
+def inorder(root):
+    if root=='.':
+        return 
+    lst=tree[root]
+    inorder(lst[0])
+    print(root,end='')
+    inorder(lst[1])
+
+def postorder(root):
+    if root=='.':
         return
-    else:
-        inorder(getLeftChild(root))
-        print(getRootVal(root),end='')
-        inorder(getRightChild(root))
-def postorder(root):#후위순회
-    if not root:
-        return
-    else:
-        postorder(getLeftChild(root))
-        postorder(getRightChild(root))
-        print(getRootVal(root),end='')
+    lst=tree[root]
+    postorder(lst[0])
+    postorder(lst[1])
+    print(root,end='')
 
-
-print('in',inorder(r))
-print('post',postorder(r))
-print('pre',preoder(r))
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+print()
