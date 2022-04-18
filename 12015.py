688838c689e4a8376d878a9272faf9526e783b43
yhLee11 <mrncd1@gmail.com>
2022-04-18 12:40:41 +0900

    binarySearch

--- 12015.py
+++ 12015.py
@@ -2,3 +2,21 @@ import sys
 input=sys.stdin.readline
 n=int(input())
 A=list(map(int,input().split()))
+LIS=[0]
+for a in A:
+    if a>LIS[-1]:
+        LIS.append(a)
+    else:
+        left=0
+        right=len(LIS)
+        while left<right:
+            mid=(left+right)//2
+            if LIS[mid]<a:
+                left=mid+1
+            else:#LIS[mid]>=a
+                right=mid
+        #     print(left,mid,right)
+        # print(LIS)
+        LIS[right]=a
+
+print(len(LIS)-1)
