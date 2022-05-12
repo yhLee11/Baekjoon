5ac1f493a30cc23f6baf99f03d784bde57451b06
yhLee11 <mrncd1@gmail.com>
2022-05-13 00:09:31 +0900

    dijkstra

--- 10282.py
+++ 10282.py
@@ -1,3 +1,4 @@
+from re import L
 import sys
 import heapq as hq
 input=sys.stdin.readline
@@ -8,18 +9,25 @@ for _ in range(tc):
     for _ in range(d):
         a,b,s=map(int,input().split())
         com[b].append((a,s))
-    
-    cnt=[]
+
     q=[]
     dist=[float('inf') for _ in range(n+1)]
+    dist[c]=0
     hq.heappush(q,(0,c))
     while q:
         cdist,cnode=hq.heappop(q)
-        cnt.append(cnode)
         if dist[cnode]<cdist:continue
         for nn,nd in com[cnode]:
             cost=nd+cdist
             if cost<dist[nn]:
                 dist[nn]=cost
                 hq.heappush(q,(cost,nn))
-    print(len(set(cnt)),dist[-1])
+
+    cnt=0
+    maxnum=0
+    for t in dist:
+        if t!=float("inf"):
+            maxnum=max(maxnum,t)
+            cnt+=1
+
+    print(cnt,maxnum)
