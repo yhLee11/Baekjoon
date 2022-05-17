0c48498c99553991c3fef80d71f2fac8ea7f1068
yeonheelee <mrncd1@gmail.com>
2022-05-17 14:47:00 +0900

    dijkstra

--- 1967.py
+++ 1967.py
@@ -1,42 +1,31 @@
 import sys
-sys.setrecursionlimit(10**9)
+import heapq as hq
 input=sys.stdin.readline
 n=int(input())
-tree=[[] for _ in range(n+1)]
+graph=[[] for _ in range(n+1)]
 
-for i in range(n-1):
-    p,c,e=map(int,input().split())
-    tree[p].append([c,c])
-    tree[c].append([p,c])
+for _ in range(n-1):
+    a,b,c=map(int,input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c])
 
-weights=[0 for i in range(n+1)]
-
-def dfs(start,tree,weights):
-    for c,e in tree[start]:
-        if weights[c]==0:
-            weights[c]=weights[start]+e
-            dfs(c,tree,weights)
-dfs(1,tree,weights)
-weights[1]=0
-maxnum=0
-maxidx=0
-for i in range(len(weights)):
-    if maxnum<weights[i]:
-        maxnum=weights[i]
-        maxidx=i
-res=[0 for _ in range(n+1)]
-dfs(maxidx,tree,res)
-res[maxidx]=0
-print(max(res))
-"""
-알고리즘 이해
-:일단 트리의 지름을 구하려면 가장 끝 노드끼리 거리가 될거다.
-거기서도 가장 큰 가중치(거리)를 가지는 것이 최종 지름이 된다.
-
-1. 루트 1번을 시작으로 해서 가장 긴(먼 거리)에 있는
-즉, 가중치가 큰 끝 노드를 DFS 탐색으로 찾는다.
-2. 찾은 끝 노드를 다시 루트노드로 설정해 DFS 탐색을 한다.
-3. 탐색하며 더한 가중치 값들 중 최댓값을 출력하면, 트리의 지름이 된다.
-
-출처: https://fullmoon1344.tistory.com/114 [태야]
-"""
+def dijkstra(start):
+    dist=[float("inf") for _ in range(n+1)]
+    dist[start]=0
+    q=[]
+    hq.heappush(q,(0,start))
+    while q:
+        cdist,cnode=hq.heappop(q)
+        if dist[cnode]<cdist:continue
+        for nn,nd in graph[cnode]:
+            cost=nd+cdist
+            if cost<dist[nn]:
+                dist[nn]=cost
+                hq.heappush(q,(cost,nn))
+    return dist
+d=dijkstra(1)
+md=max(d[1:])
+md_idx=d.index(md)
+ans=max(dijkstra(md_idx)[1:])
+#가장 긴 구간을 포함하는 도착지로부터 다시 다익스트라 알고리즘 사용
+print(ans)
