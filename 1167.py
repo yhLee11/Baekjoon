aa12d1500b859b34e242ec419f2c2e30afd870aa
yeonheelee <mrncd1@gmail.com>
2022-05-17 15:17:06 +0900

    dijkstra

--- 1167.py
+++ 1167.py
@@ -1,28 +1,32 @@
 import sys
+import heapq as hq
 input=sys.stdin.readline
 v=int(input())
-tree=[[0]*v for _ in range(v)]
-maxlen=-sys.maxsize
-stk=[]
-visit=[False for _ in range(v)]
-cnt=0
-#1 - 3
-#2 - 4
-#3 - 1 4
-#4 - 2 3 5
-#5 - 4
-def dfs(s):
-    stk=[]
-    stk.append(tree[s].reverse())
-    visit=[False for _ in range(v)]
-    while stk:
-        node
+graph=[[] for _ in range(v+1)]
+for _ in range(v):
+    lst=list(map(int,input().split()))
+    n=lst[0]
+    lst=lst[1:-1]
+    for i in range(0,len(lst),2):
+        graph[n].append([lst[i],lst[i+1]])
 
-# print(tree)
-for i in range(v):
-    n=list(map(int,input().split()))
-    start=n[0]
-    for j in range(1,len(n)-1,2):
-        tree[start-1][n[j]-1]=n[j+1]
-# print(tree)
+def dijkstra(start):
+    dist=[float('inf') for _ in range(v+1)]
+    q=[]
+    dist[start]=0
+    hq.heappush(q,(0,start))
+    while q:
+        cdist,cnode=hq.heappop(q)
+        if dist[cnode]<cdist:continue
+        for nn,nd in graph[cnode]:
+            cost=nd+dist[cnode]
+            if dist[nn]>cost:
+                dist[nn]=cost
+                hq.heappush(q,(cost,nn))
+    return dist
 
+d=dijkstra(1)[1:]
+d_max=max(d)
+d_idx=d.index(d_max)
+ans=max(dijkstra(d_idx+1)[1:])
+print(ans)
