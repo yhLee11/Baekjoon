9cdd5097538599fd51f2fb561223211a9810e7c3
yhLee11 <mrncd1@gmail.com>
2022-05-20 13:01:16 +0900

    bfs

--- 16234.py
+++ 16234.py
@@ -1,3 +1,4 @@
+from itertools import count
 import sys
 from collections import deque
 input=sys.stdin.readline
@@ -7,39 +8,37 @@ dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 visit=[[False]*N for _ in range(N)]
 flag=False
+
 def bfs(i,j):
-    cnt=1
     move=[(i,j)]
-    all=country[i][j]
     q=deque()
-    q.append((i,j,cnt))
+    q.append((i,j))
     while q:
-        x,y,c=q.popleft()
+        x,y=q.popleft()
         for i in range(4):
             nx=dx[i]+x
             ny=dy[i]+y
             if 0<=nx<N and 0<=ny<N and not visit[nx][ny]:
                 if L<=abs(country[x][y]-country[nx][ny])<=R:
                     visit[nx][ny]=True
-                    q.append((nx,ny,c+1))
-                    all+=country[nx][ny]
+                    q.append((nx,ny))
                     move.append((nx,ny))
-    global flag
-    if c==1:flag=True
-    num=int(all/c)
-    for m1,m2 in move:
-        country[m1][m2]=num
-
-flag=False
-ans=0
+    return move
+cnt=0
 while True:
     visit=[[False]*N for _ in range(N)]
+    isTrue=False
     for i in range(N):
         for j in range(N):
             if not visit[i][j]:
-                bfs(i,j)
-                
-    if flag:
+                visit[i][j]=True
+                tmp=bfs(i,j)
+                if len(tmp)>1:
+                    isTrue=True
+                    num=sum([country[x][y] for x,y in tmp])//len(tmp)
+                    for x,y in tmp:
+                        country[x][y]=num
+    if not isTrue:
         break
-    ans+=1
-print(ans)
+    cnt+=1                
+print(cnt)
