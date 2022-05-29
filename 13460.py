357636cde2acdff4406356efc5e898542cde61dd
yhLee11 <mrncd1@gmail.com>
2022-05-29 22:32:44 +0900

    bfs ing..

--- 13460.py
+++ 13460.py
@@ -1,7 +1,17 @@
 import sys
+from collections import deque
 input=sys.stdin.readline
 h,w=map(int,input().split())
-arr=[list(input().strip()) for _ in range(h)]
+s=[list(input().strip()) for _ in range(h)]
 dx=[0,0,1,-1]
 dy=[1,-1,0,0]
 
+def bfs(i,j):
+    q=deque([(i,j)])
+    while q:
+        x,y=q.popleft()
+        for i in range(4):
+            nx=dx[i]+x
+            ny=dy[i]+y
+            
+            if s[nx][ny]
