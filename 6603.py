1a8e5684f4c251348b0872f10712e467489ac3e5
yhLee11 <mrncd1@gmail.com>
2022-06-09 10:23:40 +0900

    backtracking

---
+++ 6603.py
@@ -0,0 +1,19 @@
+from gettext import dpgettext
+import sys
+input=sys.stdin.readline
+def dfs(start,depth):
+    if depth==6:
+        print(' '.join(map(str,combi)))
+        return
+    for i in range(start,len(s)):
+        combi[depth]=s[i]
+        dfs(i+1,depth+1)
+
+k=-1
+combi=[0 for i in range(6)]
+while k!=0:
+    lst=list(map(int,input().split()))
+    k=lst[0]
+    s=lst[1:]
+    dfs(0,0)
+    print()
