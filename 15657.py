e1c3755e03e0ec988ce90612253b0a37165290d6
yhLee11 <mrncd1@gmail.com>
2022-06-10 12:31:31 +0900

    backtracking

---
+++ 15657.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))
+arr.sort()
+s=[]
+def dfs(depth):
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    for i in range(n):
+        if depth==0 or s[depth-1]<=arr[i]:
+            s.append(arr[i])
+            dfs(depth+1)
+            s.pop()
+dfs(0)
