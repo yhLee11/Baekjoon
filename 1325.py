d6a2647128a234a492adbf1bb53332a776fb9cdf
yhLee11 <mrncd1@gmail.com>
2022-09-15 15:41:18 +0900

    bfs

--- 1325.py
+++ 1325.py
@@ -8,27 +8,30 @@ for _ in range(m):
     a,b=map(int,input().split())
     trust[b].append(a)
 
+mx=0
+res=[]
+
 def bfs(start):
     q=deque()
     q.append(start)
-    visit=[False]*(n+1)
+    v=[False]*(n+1)
+    v[start]=True
     cnt=1
     while q:
-        node=q.popleft()
-        for v in trust[node]:
-            if not visit[v]:
-                q.append(v)
-                visit[v]=True
+        cur=q.popleft()
+
+        for nxt in trust[cur]:
+            if not v[nxt]:
+                v[nxt]=True
+                q.append(nxt)
                 cnt+=1
     return cnt
-res=[]
-maxcnt=-sys.maxsize
-for i in range(1,n+1):
-    tmp=bfs(i)
-    maxcnt=max(maxcnt,tmp)
-    res.append((i,tmp))
-ans=[]
-for i,cnt in res:
-    if cnt==maxcnt: 
-        ans.append(i)
-print(*ans)
+
+for st in range(1,n+1):
+    cnt=bfs(st)
+    mx=max(mx,cnt)
+    res.append((st,cnt))
+
+for s,c in res:
+    if c==mx:
+        print(s,end=' ')
