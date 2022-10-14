fef5955fcba65ec6f2f93be124504fbd3261e427
yhLee11 <mrncd1@gmail.com>
2022-10-14 14:48:55 +0900

    bfs, dfs

--- 9372.py
+++ 9372.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 input=sys.stdin.readline
 T=int(input())
@@ -11,11 +12,26 @@ for _ in range(T):
     # print(tree)
     v=[False]*(N+1)
 
+    def bfs(start):
+        q=deque()
+        q.append(start)
+        v[start]=True
+        cnt=0
+        while q:
+            cur=q.popleft()
+            for i in tree[cur]:
+                if not v[i]:
+                    q.append(i)
+                    v[i]=True
+                    cnt+=1
+        return cnt
+
     def dfs(start,cnt):
         v[start]=1
         for i in tree[start]:
             if not v[i]:
                 cnt=dfs(i,cnt+1)
         return cnt
-    res=dfs(1,0)
+    # res=dfs(1,0)
+    res=bfs(1)
     print(res)
