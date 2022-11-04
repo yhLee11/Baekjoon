da6db4cdb51f53f012df0a4cf7b1380855837653
yhLee11 <mrncd1@gmail.com>
2022-11-04 10:49:14 +0900

    floyd

--- 1058.py
+++ 1058.py
@@ -1,20 +1,15 @@
-n=int(input())
-a=[]
-v=[]
-v=[[0]*n for _ in range(n)]
-stack=[]
-for i in range(n):
-    s=input()
-    
-#    for idx,value in enumerate(s):
-
-#        if value[idx]=='N':
-#            a.append(0)
-#        else:
-#            a.appen(1)
-
-
-#def dfs(a):
-
-#    stack=[a[i]*]
-#    while True:
+import sys
+input=sys.stdin.readline
+N=int(input())
+lst=[input().strip() for _ in range(N)]
+v=[[0]*N for _ in range(N)]
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if i==j:continue
+            if lst[i][j]=='Y' or (lst[i][k]=='Y' and lst[k][j]=='Y'):
+                v[i][j]=1
+res=0
+for i in range(N):
+    res=max(res,sum(v[i]))
+print(res)
